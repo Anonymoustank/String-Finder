93446b8abf08a4d43cd037a158e85a5b374b08ba
Anonymoustank <54193508+Anonymoustank@users.noreply.github.com>
2020-04-28 12:24:34 -0600

    Add files via upload

---
+++ String Finder.py
@@ -0,0 +1,35 @@
+word = "Pranav"
+
+word = word.lower()
+
+target = "pranav"
+
+target = target.lower()
+
+new_target = target[::-1]
+
+found = True
+
+in_loop = 0
+
+for index,char in enumerate(word):
+    if char == new_target[0]:
+        in_loop += 1
+        for x in range(0, len(new_target)):
+            if new_target[x] == word[index-x]:
+                if found == False:
+                    found = False
+                else:
+                    found = True
+            else:
+                found = False
+    
+
+
+if found == True and in_loop > 0:
+    print("Target was found within string")
+else:
+    print("Target wasn't found within string")
+            
+            
+            
